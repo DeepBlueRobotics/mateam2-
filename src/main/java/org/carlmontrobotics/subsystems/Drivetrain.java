8efe7da391ec9fcddf9c9d53c98e95227a753b52
Talya36 <823501@seq.org>
2024-10-20 17:48:35 -0700

    added constants and arcade drive

--- src/main/java/org/carlmontrobotics/subsystems/Drivetrain.java
+++ src/main/java/org/carlmontrobotics/subsystems/Drivetrain.java
@@ -9,23 +9,34 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static org.carlmontrobotics.Constants.drivetrainC.*;
+import static org.carlmontrobotics.Constants.OI.*;
+
 
 
 public class Drivetrain extends SubsystemBase {
-  CANSparkMax rightWheels = MotorControllerFactory.createSparkMax(1, MotorConfig.NEO);
-  CANSparkMax leftWheels = MotorControllerFactory.createSparkMax(2, MotorConfig.NEO);
-  private XboxController XboxController1 = new XboxController(0);
+  CANSparkMax rightWheels = MotorControllerFactory.createSparkMax(right_motor_port, MotorConfig.NEO);
+  CANSparkMax leftWheels = MotorControllerFactory.createSparkMax(left_motor_port, MotorConfig.NEO);
+  private XboxController XboxController1 = new XboxController(XBOX_CONTROLLER_PORT);
   
   /** Creates a new Drivetrain. */
   public Drivetrain() {
 
   }
 
-  public void tankDrive() {
+  public void tankDrive(double right_Y, double left_Y) {
     double rightY = XboxController1.getRightY();
     double leftY = XboxController1.getLeftY();
-    rightWheels.set(rightY);
-    leftWheels.set(leftY);
+    rightWheels.set(right_Y);
+    leftWheels.set(left_Y);
+  }
+
+  public void arcadeDrive(double speed, double turnX) {
+    double turnX = XboxController1.getRightX();
+    double speed = XboxController1.getLeftY();
+    rightWheels.set(speed + turnX);
+    leftWheels.set(speed - turnX);
+
   }
 
   @Override

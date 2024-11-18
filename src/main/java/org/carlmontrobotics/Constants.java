200010b8f835a0bc7d0777b20240a30bca8914eb
stwiggy <karenaxling@gmail.com>
2024-11-17 16:39:40 -0800

    this gonna work

--- src/main/java/org/carlmontrobotics/Constants.java
+++ src/main/java/org/carlmontrobotics/Constants.java
@@ -3,11 +3,8 @@ package org.carlmontrobotics;
 import edu.wpi.first.wpilibj.XboxController.Button;
 
 public final class Constants {
-    // public static final class Drivetrain {
-    //     public static final double MAX_SPEED_MPS = 2;
-    // }
+
     public static final class OI {
-        //ublic static final int port = 0;
         public static final int XBOX_CONTROLLER_PORT = 0;
     }
 
--- src/main/java/org/carlmontrobotics/subsystems/Door.java
+++ src/main/java/org/carlmontrobotics/subsystems/Door.java
@@ -17,17 +17,10 @@ import edu.wpi.first.wpilibj.XboxController;
 
 
 public class Door extends SubsystemBase {
-    CANSparkMax doorMotor = MotorControllerFactory.createSparkMax(DoorConstants.doorPort, MotorConfig.NEO);
-    RelativeEncoder encoder = doorMotor.getEncoder();
-    private XboxController xboxController2 = new XboxController(2); 
-  /** Creates a new Drivetrain. */
-  /*public void doorOpen() {
-   double leftY2 = xboxController2.getLeftY();
-    doorMotor.set(leftY2 * DoorConstants.doorSpeed);
-  }
-  public void doorOpenButton() {
-    
-  }*/
+  CANSparkMax doorMotor = MotorControllerFactory.createSparkMax(DoorConstants.doorPort, MotorConfig.NEO);
+  RelativeEncoder encoder = doorMotor.getEncoder();
+
+  public Door() {}
 
   public void doorOpenButton(){
     doorMotor.set(DoorConstants.doorSpeed);
@@ -38,13 +31,11 @@ public class Door extends SubsystemBase {
   public void doorStopButton(){
     doorMotor.set(0);
   }
+
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
     SmartDashboard.putNumber("Position", encoder.getPosition());
-    //doorOpen();
-    //doorOpenButton();
-    //doorCloseButton();
+
   }
 }
   
